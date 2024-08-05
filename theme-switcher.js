8ee0618e464673712834f549b1986ef91ffef504
b00yahg <130240131+b00yahg@users.noreply.github.com>
2024-08-05 01:32:52 -0500

    Update theme-switcher.js

--- theme-switcher.js
+++ theme-switcher.js
@@ -10,13 +10,21 @@ const backgroundImages = [
   'https://live.staticflickr.com/65535/52259221868_e86daccb7d_6k.jpg',
   'https://live.staticflickr.com/65535/52211883799_abf0e1909c_6k.jpg',
   'https://static.wikia.nocookie.net/spelljammer/images/0/04/Hs-2007-16-a-large_web.jpg/revision/latest?cb=20210610184324', 
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrDAqY61sqjSDiubL3bbIcolXsvGhMAFtprw&s',
+  'https://i.ibb.co/DQkcyhk/OH-time-to-spelljammer-for-sure-waifu2x-noise0-scale4x.png',
   'https://assetsio.gnwcdn.com/spelljammer-dnd-2e-artwork.png?width=1920&height=1920&fit=bounds&quality=80&format=jpg&auto=webp',
   'https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/04/Dungeons-Dragons-Spelljammer-Pirates-Cropped.jpg',
   'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcm0yMDktYWRqLTEzLWcuanBn.jpg',
   'https://images.squarespace-cdn.com/content/v1/5cd1c8e165a707bd044a52b7/1573971953743-NC8MKRWBMOHZZSUFZ4A0/ke17ZwdGBToddI8pDm48kJRqFJ19D4P4EwsC9z3fiewUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYy7Mythp_T-mtop-vrsUOmeInPi9iDjx9w8K4ZfjXt2dkV64dCjSK7Zaaf7dwPYPO_gHf_vjqrS5WJoq1nmwotrP7cJNZlDXbgJNE9ef52e8w/Maria+Angelica+Beatriz+Ramirez+Morelos-Sky+Voyage.jpg?format=1500w',
   'https://static1.srcdn.com/wordpress/wp-content/uploads/2022/09/Spelljammer-Dead-God-Cover.jpg?q=50&fit=crop&w=1100&h=618&dpr=1.5',
   // Add more image URLs as needed
+];
+
+// Array of custom names for the backgrounds
+const backgroundNames = [
+  "Spelljammer 1", "Spelljammer 2", "Spelljammer 3", "Spelljammer 4", "Spelljammer 5",
+  "NASA 1", "NASA 2", "NASA 3", "NASA 4", "NASA 5",
+  "Old School 1", "Old School 2", "Old School 3",
+  "Misc 1", "Misc 2", "Misc 3"
 ];
 
 // Function to switch between light and dark mode
@@ -32,16 +40,18 @@ function changeBackgroundImage(index) {
   localStorage.setItem('backgroundImageIndex', index);
 }
 
-// Create and append background image selector
+// Updated function to create and append background image selector
 function createBackgroundImageSelector() {
   const select = document.createElement('select');
   select.id = 'bgImageSelect';
+  
   backgroundImages.forEach((_, index) => {
     const option = document.createElement('option');
     option.value = index;
-    option.textContent = `Background ${index + 1}`;
+    option.textContent = backgroundNames[index] || `Background ${index + 1}`;
     select.appendChild(option);
   });
+  
   select.addEventListener('change', (e) => changeBackgroundImage(e.target.value));
   document.body.appendChild(select);
 }
